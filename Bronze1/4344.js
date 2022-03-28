15fe74eab525a9dc56dc59138c47671bf51effca
simonkim1 <sevencuter@gmail.com>
2022-03-28 10:32:55 +0900

    백준_4344_javascript

---
+++ Bronze1/4344.js
@@ -0,0 +1,8 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
+const input  = require("fs").readFileSync("testCase/testCase.txt").toString();
+
+function iGuessOverTheAvg(input) {
+    
+}
+
+iGuessOverTheAvg(input);
