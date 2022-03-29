1352494dcf63177120db7bab31050ecbc2602e0b
simonkim1 <sevencuter@gmail.com>
2022-03-29 14:38:25 +0900

    백준_4344_javascript

--- Bronze1/4344.js
+++ Bronze1/4344.js
@@ -1,8 +1,27 @@
 // const input  = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
-const input  = require("fs").readFileSync("testCase/testCase.txt").toString();
+const input  = require("fs").readFileSync("testCase/testCase.txt").toString().trim().split("\n");
 
 function iGuessOverTheAvg(input) {
-    
+    for(let i = 1; i <input.length; i++) {
+        let testCase = input[i].split(" ");
+        let numOfStudents = testCase[0];
+        let sum = 0;
+        
+        testCase.shift();
+
+        for(let j = 0; j < numOfStudents; j++) {
+            sum += Number(testCase[j]);
+        }
+
+        let avg = sum / numOfStudents;
+        let overAvgCnt = 0;
+
+        for(let k = 0; k < numOfStudents; k++) {
+            if(testCase[k] > avg)
+                overAvgCnt++;
+        }
+        console.log(`${(overAvgCnt / testCase.length * 100).toFixed(3)}%`);
+    }
 }
 
 iGuessOverTheAvg(input);
