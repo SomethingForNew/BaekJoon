fb3cb71041e229637b9709665f117d810518c877
simonkim1 <sevencuter@gmail.com>
2022-03-30 11:01:59 +0900

    백준_10773_javascript

---
+++ Silver5/10773.js
@@ -0,0 +1,28 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().trim().split("\r\n");
+const input  = require("fs").readFileSync("testCase/testCase.txt").toString().trim().split("\r\n");
+
+function zero(input) {
+    let testCaseCnt = input[0];
+    let a = [];
+
+    for(let i = 1; i <= testCaseCnt; i++) {
+        if(input[i] != "0") {
+            a.push(input[i]);
+        } else if (input[i] === "0") {
+            a.pop();
+        }
+        // console.log(a);
+    }
+
+    if(a.length < 1) {
+        return console.log(0);
+    } else {
+        let answer = 0;
+        a.forEach(element => {
+            answer += +element;
+        })
+        return console.log(answer);
+    }
+}
+
+zero(input);
---
+++ Silver5/2941.js
@@ -0,0 +1,11 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString();
+const input = "ljes=njak".toString();
+
+function croatiaLanguage(input) {
+    let wordsArr = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+    
+    console.log(input)
+}
+
+
+croatiaLanguage(input);
