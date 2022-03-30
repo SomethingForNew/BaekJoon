84d0fb7adb487cc7918a9ddf83b4d2d1f61f0b5e
simonkim1 <sevencuter@gmail.com>
2022-03-30 18:25:11 +0900

    백준_2941_javascript

--- Silver5/2941.js
+++ Silver5/2941.js
@@ -1,11 +1,29 @@
-// const input  = require("fs").readFileSync("/dev/stdin").toString();
-const input = "ljes=njak".toString();
+// const input = require("fs").readFileSync("/dev/stdin", "utf-8").trim().toString();
+const input = "ljes=njak".trim().toString();
+// const input = "ddz=z=".trim().toString();
+// const input = "nljj".trim().toString();
+// const input = "c=c=".trim().toString();
+// const input = "dz=ak".trim().toString();
 
 function croatiaLanguage(input) {
-    let wordsArr = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+    // ▽문자열을 이용▽
+    let RegExp = /c=|c-|dz=|d-|lj|nj|s=|z=/g;
+    let answer = input.replace(RegExp, " ");
     
-    console.log(input)
-}
+    console.log(answer.length);
 
+    // ▽반복문을 이용▽
+    // 반복문을 이용한 코드는 구글에서 찾음
+    // let wordsArr = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
+    // let answer   = 0;
+
+    // for(let i = 0; i < wordsArr.length; i++) {
+    //     while(input !== input.replace(wordsArr[i], "")) {
+    //         answer ++;
+    //         input = input.replace(wordsArr[i], " ");
+    //     }
+    // }
+    // console.log( answer + input.split(" ").join("").length );
+}
 
 croatiaLanguage(input);
