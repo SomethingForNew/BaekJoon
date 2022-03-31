183c2d347242d0d88411025a1019e7524afb93d8
simonkim1 <sevencuter@gmail.com>
2022-03-31 14:08:30 +0900

    백준_2839_javascript

---
+++ Bronze1/2839.js
@@ -0,0 +1,30 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().trim();
+// const input = "18".toString().trim();
+// const input = "4".toString().trim();
+// const input = "6".toString().trim();
+// const input = "9".toString().trim();
+const input = "11".toString().trim();
+
+function sugerDelivery(input) {
+    let kg = Number(input);
+    let bongji = 0;
+
+    while(true) {
+        if(kg % 5 === 0) {
+            bongji += (kg / 5);
+            break;
+        }
+
+        if(kg <= 0) {
+            bongji = -1;
+            break;
+        }
+
+        kg = kg - 3;
+        bongji++;
+    }
+
+    console.log(bongji);
+}
+
+sugerDelivery(input);
