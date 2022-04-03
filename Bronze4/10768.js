c6555050bea3c191e1ae61a9e64b2b510468ae29
simonkim1 <sevencuter@gmail.com>
2022-04-03 22:15:49 +0900

    Create 10768.js
    백준_10768_javascript

---
+++ Bronze4/10768.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const [month, day] = fs.readFileSync("/dev/stdin").toString().trim().split('\n').map(Number);
+const answer = (month, day) => {
+    if(month > 2)
+        return "After";
+    if(month < 2)
+        return "Before";
+    if(month === 2) { if(day > 18)
+        return "After";
+    if(day < 18)
+        return "Before";
+    if(day === 18)
+        return "Special"
+                    }
+}
+console.log(answer(month, day));
