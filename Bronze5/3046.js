16b900572b1bc3e0ad9ba3a56a13074599f70cf6
simonkim1 <sevencuter@gmail.com>
2022-04-04 09:47:02 +0900

    백준_3046_javascript

---
+++ Bronze5/3046.js
@@ -0,0 +1,13 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+// const input = "11 15".toString().split(" ");
+const input = "4 3".toString().split(" ");
+
+function getR2Number(input) {
+    let r1 = Number(input[0]);
+    let s  = Number(input[1]);
+    let answer = (s * 2) - r1;
+
+    console.log(answer);
+}
+
+getR2Number(input);
