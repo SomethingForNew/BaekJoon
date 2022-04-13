ce2d503db2fa5412b54026895be4470f7a93e196
simonkim1 <sevencuter@gmail.com>
2022-04-13 16:04:26 +0900

    백준_10989_java

---
+++ Silver4/10989.java
@@ -0,0 +1,21 @@
+public class 10989 {
+    public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+		int arr[] = new int[N];
+
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(arr);
+
+		for(int num : arr) {
+			sb.append(num).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+}
