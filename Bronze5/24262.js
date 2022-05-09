79afb1efbc2ba144dc193c5d33ba157e886f940a
simonkim1 <sevencuter@gmail.com>
2022-05-09 13:46:13 +0900

    백준_24262_javascript

---
+++ Bronze5/24262.js
@@ -0,0 +1,9 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().trim();
+const input  = "1".toString().trim();
+
+function algorithmClass(input) {
+    console.log(1);
+    console.log(0);
+}
+
+algorithmClass(input);
