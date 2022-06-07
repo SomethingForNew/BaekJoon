8462f4cc7c9f8c46736a7a2694a94d5bcfc1b2d1
simonkim1 <sevencuter@gmail.com>
2022-06-07 22:25:45 +0900

    백준_2743.js

---
+++ Bronze5/2743.js
@@ -0,0 +1,8 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const input = "pulljima".toString().trim();
+
+function checkTheLength(input) {
+    console.log(input.length);
+}
+
+checkTheLength(input);
