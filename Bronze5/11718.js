e3a1ffbc4408b0694c7bbc9f30af01ec2cab30b8
simonkim1 <sevencuter@gmail.com>
2022-06-12 19:11:02 +0900

    백준_11718.js

---
+++ Bronze5/11718.js
@@ -0,0 +1,8 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString();
+const input = require("fs").readFileSync("testCase/testCase.txt").toString();
+
+function print(input) {
+    console.log(input);
+}
+
+print(input);
