2e06470c8c028580b1ececc94d726ce598fa6666
simonkim1 <sevencuter@gmail.com>
2022-06-18 14:53:57 +0900

    백준_2738.js

--- Bronze5/2738.js
+++ Bronze5/2738.js
@@ -2,22 +2,20 @@
 const input = require("fs").readFileSync("./testCase/testCase.txt").toString().trim().split("\n");
 
 function matrixSum(input) {
-    const [N, M] = input.shift().split(" ").map((element) => Number(element));
-    let testCase1 = input.splice(0, N);
-    let testCase2 = input.splice(0, M);
-    let answerArr = [];
-
-    for(let i = 0; i < testCase1.length; i++) {
-        let tempArr = [];
-
-        for(let j = 0; j < testCase2.length; j++) {
-            tempArr.push(Number(testCase1[i].split(" ")[j]) + Number(testCase2[i].split(" ")[j]));
-        }
-
-        answerArr.push(tempArr);
-    }
-
-    console.log(answerArr.join("\n").replaceAll(",", " "));
+    let number = input
+		.shift()
+		.split(" ")
+		.map((el) => Number(el));
+	input = input.map((el) => el.split(" ").map((el) => Number(el)));
+	let arr1 = input.splice(0, number[0]);
+	let result = arr1.map((el1, idx1) =>
+		el1.map((el2, idx2) => el2 + input[idx1][idx2])
+	);
+	let value = [];
+	result.forEach((el) => {
+		value.push(el.join(" "));
+	});
+	console.log(value.join("\n"));
 }
 
 matrixSum(input);
