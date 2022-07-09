d8f0dea0908740bddf7253cd37bac9172e8e22b2
simonkim1 <sevencuter@gmail.com>
2022-07-09 17:42:50 +0900

    백준_14652.js

--- Bronze5/14652.js
+++ Bronze5/14652.js
@@ -2,7 +2,7 @@
 const input = "6 4 14".toString().trim().split(' ').map(Number);
 
 function hanhwa(input) {
-    console.log(parseInt(input[2] / input[1]), parseInt(input[2]%input[1]));
+    console.log(parseInt(input[2] / input[1]), parseInt(input[2] % input[1]));
 }
 
 hanhwa(input);
