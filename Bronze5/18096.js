a638dfbb78f9bde96ca02db251a607c6947086a8
simonkim1 <sevencuter@gmail.com>
2022-07-11 14:22:31 +0900

    백준_18096.js

---
+++ Bronze5/18096.js
@@ -0,0 +1,8 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString();
+const input = "3".toString();
+function Арифметическаямагия(input) {
+    const N = Number(input);
+    console.log(1);
+}
+
+Арифметическаямагия(input);
