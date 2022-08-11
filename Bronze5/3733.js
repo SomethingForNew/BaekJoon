b45d9588de724cf15ddd1b4a3f9fd1928854eeeb
simonkim1 <sevencuter@gmail.com>
2022-08-11 18:16:51 +0900

    백준_3733.js

--- Bronze5/3733.js
+++ Bronze5/3733.js
@@ -1,8 +1,7 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const input = require("fs").readFileSync("testCase/testCase.txt").toString().split("\n");
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = require("fs").readFileSync("testCase/testCase.txt").toString().trim().split("\n");
 
 function shares(input) {
-    let arr = [];
     for(let i = 0; i < input.length; i++) {
         const [N, S] = input[i].split(" ").map(v => Number(v));
 
