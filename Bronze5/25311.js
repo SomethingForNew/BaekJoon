80e838d0814c6b0dd66a90b32c86ba82f100bfce
simonkim1 <sevencuter@gmail.com>
2022-08-12 17:46:11 +0900

    백준_25311.js

---
+++ Bronze5/25311.js
@@ -0,0 +1,8 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString();
+const input = "2018".toString();
+
+function easyUCPC(input) {
+    return "A";
+}
+
+console.log(easyUCPC(input));
