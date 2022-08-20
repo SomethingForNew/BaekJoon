b50a1c73ae3dc9390439457a71c1480cbfc3aacc
simonkim1 <sevencuter@gmail.com>
2022-08-20 17:28:27 +0900

    백준_24082.js

---
+++ Bronze5/24082.js
@@ -0,0 +1,8 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().trim();
+const input = "4".toString().toString().trim();
+
+function 立方体(input) {
+    return Number(input) ** 3;
+}
+
+console.log(立方体(input));
