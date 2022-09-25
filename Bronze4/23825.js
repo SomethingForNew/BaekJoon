cdce65d18c53ddf206fa22573c39d8c537204637
simonkim1 <sevencuter@gmail.com>
2022-09-25 16:07:50 +0900

    백준_23825.js

--- Bronze4/23825.js
+++ Bronze4/23825.js
@@ -1,14 +1,8 @@
-const { off } = require("process");
-
-// const input  = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
 const input = "4 5".toString().trim().split(" ");
 
 function SASA(input) {
-    const N = +input[0];
-    const M = +input[1];
-
-    console.log(N, M);
-    console.log(5 % 2);
+    return Math.floor(Math.min.apply(null, input) / 2);
 }
 
-SASA(input);
+console.log(SASA(input));
