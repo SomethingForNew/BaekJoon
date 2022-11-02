195b61b3e2f53f94224ced58e28e8a2ab856321a
simonkim1 <sevencuter@gmail.com>
2022-11-02 09:23:09 +0900

    백준_21598.js

---
+++ Bronze5/21598.js
@@ -0,0 +1,10 @@
+// const input  = require("fs").readFileSync("/dev/stdin").toString().trim();
+const input = "2".toString().trim();
+
+function sciComLove(input) {
+    for(let i = 0; i < input; i++) {
+        console.log("SciComLove");
+    }
+}
+
+sciComLove(input);
