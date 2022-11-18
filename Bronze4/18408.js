21528a7c1d3c4d9bb6e3469fba6488a4b009eb46
simonkim1 <sevencuter@gmail.com>
2022-11-18 13:47:54 +0900

    백준_18408.js

--- Bronze4/18408.js
+++ Bronze4/18408.js
@@ -3,14 +3,11 @@ const input = "1 2 2".toString().trim();
 
 function threeIntegers(input) {
     const testCaseArr = input.split(" ").map(Number);
-    let one = 0;
-    let two = 0;
+    
+    const arr = [0, 0, 0];
+    testCaseArr.forEach(testCase => ++arr[testCase]);
 
-    for(let i = 0; i < testCaseArr.length; i++) {
-        testCaseArr[i] % 2 !== 0 ? one++ : two++;
-    }
-
-    console.log(one > two ? 1 : 2);
+    console.log(arr[1] > arr[2] ? 1 : 2);
 }
 
 threeIntegers(input);
