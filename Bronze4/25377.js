685f2e22ded16d2db0e9a29389e36e9827311c5a
simonkim1 <sevencuter@gmail.com>
2023-01-12 17:53:57 +0900

    백준_25377.js

---
+++ Bronze4/25377.js
@@ -0,0 +1,10 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = require("fs").readFileSync("testCase/testCase.txt").toString().trim().split("\n");
+
+function bread(input) {
+    const [N, ...TEST_CASE] = input;
+    const ANSWER_ARR = [];
+
+}
+
+console.log(bread(input));
