465154c2e437eedef8356bdea8621544814451f8
simonkim1 <sevencuter@gmail.com>
2023-01-30 09:37:05 +0900

    백준_27110.js

---
+++ Bronze4/27110.js
@@ -0,0 +1,20 @@
+// const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = require("fs").readFileSync("testCase/testCase.txt").toString().trim().split("\n");
+
+function chicken(input) {
+    const [N, ...TESTCASE] = input;
+    const TESTCASEARR = TESTCASE.toString().split(" ").map(Number);
+    let answer = 0;
+
+    TESTCASEARR.forEach(element => {
+        if(element <= N) {
+            answer += element;
+        } else if(element > N) {
+            answer += Number(N);
+        }
+    });
+
+    return answer;
+}
+
+console.log(chicken(input));
