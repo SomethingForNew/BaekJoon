7ab1aea4c305a251477b55cfbed8211a997c5c96
simonkim1 <sevencuter@gmail.com>
2023-05-04 11:48:37 +0900

    백준_2753.py

---
+++ Bronze5/2753.py
@@ -0,0 +1,19 @@
+import sys
+year = int(sys.stdin.readline())
+
+# if year % 4 == 0 and year % 100 != 0:
+#     print(1)
+# elif year % 4 == 0 and year % 400 == 0:
+#     print(1)
+# else:
+#     print(0)
+
+# if-else
+# if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0):
+#     print(1)
+# else:
+#     print(0)
+
+
+# 삼항 연산자
+# print(1) if ((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0) else print(0)
