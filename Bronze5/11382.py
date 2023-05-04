bd2e6f777fbc33269f84e934cb9d9578b2843711
simonkim1 <sevencuter@gmail.com>
2023-05-04 11:29:18 +0900

    백준_11382.py

---
+++ Bronze5/11382.py
@@ -0,0 +1,10 @@
+# A, B, C = input().split()
+# print(int(A) + int(B) + int(C))
+
+# A, B, C = map(int, input().split())
+# print(A + B + C)
+
+# print(sum(map(int, input().split())))
+
+# import sys
+# print(sum(map(int, sys.stdin.readline().split())))
