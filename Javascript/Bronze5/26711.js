02d43edc15bff07179bce861f627b7e7a1c37104
simonkim1 <sevencuter@gmail.com>
2023-05-18 15:16:08 +0900

    백준

--- Javascript/Bronze5/26711.js
+++
@@ -1,8 +0,0 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n").map(Number);
-const input = require("fs").readFileSync("testCase/testCase.txt").toString().split("\n").map(BigInt);
-
-function APlusB(input) {
-    return (input[0] + input[1]).toString();
-}
-
-console.log(APlusB(input));
