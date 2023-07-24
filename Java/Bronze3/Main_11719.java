fc7231481899049e7679a6cb8fbb4a75f0fb1f9a
simonkim1 <sevencuter@gmail.com>
2023-07-24 11:43:05 +0900

    백준_11719_Java

---
+++ Java/Bronze3/Main_11719.java
@@ -0,0 +1,16 @@
+package Bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
