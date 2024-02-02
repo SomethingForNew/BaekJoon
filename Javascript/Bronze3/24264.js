d59050643951242209a17d9face6d8f003b22e78
Seunghyun Kim <sevencuter@gmail.com>
2024-02-02 15:17:10 +0900

    24264.js

---
+++ Javascript/Bronze3/24264.js
@@ -0,0 +1,20 @@
+// const n = require("fs").readFileSync("/dev/stdin").toString()
+const n = require("fs").readFileSync("./testCase.txt").toString();
+// const arr = [];
+
+// function MenOfPassion(A, n) {
+//   let sum = 0;
+
+//   for (let i = 1; i < n; i++) {
+//     for (let j = 1; j < n; j++) {
+//       sum += sum + A[i] * A[j];
+//     }
+//   }
+
+//   return sum;
+// }
+
+// console.log(MenOfPassion(arr, input));
+
+console.log(`${n ** 2}
+2`);
