10fdc41297cfd729def480ba2ddf5a5429dac724
Seunghyun Kim <sevencuter@gmail.com>
2024-02-02 17:18:23 +0900

    19532.js

---
+++ Javascript/Bronze2/19532.js
@@ -0,0 +1,14 @@
+// const [a, b, c, d, e, f] = require("fs").readFileSync("/dev/stdin").toString().split(" ").map(Number);
+const [a, b, c, d, e, f] = require("fs")
+  .readFileSync("./testCase.txt")
+  .toString()
+  .split(" ")
+  .map(Number);
+
+for (let x = -999; x <= 999; x++) {
+  for (let y = -999; y <= 999; y++) {
+    if (a * x + b * y === c && d * x + e * y === f) {
+      console.log(`${x} ${y}`);
+    }
+  }
+}
